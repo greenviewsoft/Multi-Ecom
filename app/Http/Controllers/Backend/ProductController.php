61cb089aa65ff70bd03cd89503102d203dd742f1
Md Tipu Sultan <greenviewsoft@gmail.com>
2022-11-02 19:33:14 +0600

    Load category

--- app/Http/Controllers/Backend/ProductController.php
+++ app/Http/Controllers/Backend/ProductController.php
@@ -9,6 +9,7 @@ use App\Models\SubCategory;
 use App\Models\MultiImg;
 use App\Models\Brand;
 use App\Models\Product;
+use App\Models\User;
 use Intervention\Image\Facades\Image As Image;
 
 class ProductController extends Controller
@@ -19,7 +20,10 @@ class ProductController extends Controller
     } // End Method
 
     public function AddProduct(){
-        return view('backend.product.product_add');
+        $activeVendor = User::where('status','active')->where('role','vendor')->latest()->get();
+        $brands = Brand::latest()->get();
+        $categories = Category::latest()->get();
+        return view('backend.product.product_add',compact('brands','categories','activeVendor'));
     } // End Method
 
 }
