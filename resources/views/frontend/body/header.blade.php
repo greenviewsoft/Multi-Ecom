355aa1cd5c89a197d2810fab6a2f29fa08cd0159
Md Tipu Sultan <greenviewsoft@gmail.com>
2022-11-30 15:15:46 +0600

    Frontend Category Show part 1

--- resources/views/frontend/body/header.blade.php
+++ resources/views/frontend/body/header.blade.php
@@ -211,8 +211,9 @@
 
 
 
-
-
+@php
+   $categories = App\Models\Category::orderBy('category_name','ASC')->get();
+@endphp
 
 
 
@@ -231,38 +232,22 @@
                         <div class="categories-dropdown-wrap categories-dropdown-active-large font-heading">
                             <div class="d-flex categori-dropdown-inner">
                                 <ul>
+                                    @foreach ($categories as $item)
+
+
                                     <li>
-                                        <a href="shop-grid-right.html"> <img src="{{ asset('frontend/assets/imgs/theme/icons/category-1.svg') }}" alt="" />Milks and Dairies</a>
+                                        <a href="shop-grid-right.html"> <img src="{{ asset($item->category_image) }}" alt="" />{{$item->category_name  }}</a>
                                     </li>
-                                    <li>
-                                        <a href="shop-grid-right.html"> <img src="{{ asset('frontend/assets/imgs/theme/icons/category-2.svg') }}" alt="" />Clothing & beauty</a>
-                                    </li>
-                                    <li>
-                                        <a href="shop-grid-right.html"> <img src="{{ asset('frontend/assets/imgs/theme/icons/category-3.svg') }}" alt="" />Pet Foods & Toy</a>
-                                    </li>
-                                    <li>
-                                        <a href="shop-grid-right.html"> <img src="{{ asset('frontend/assets/imgs/theme/icons/category-4.svg') }}" alt="" />Baking material</a>
-                                    </li>
-                                    <li>
-                                        <a href="shop-grid-right.html"> <img src="{{ asset('frontend/assets/imgs/theme/icons/category-5.svg') }}" alt="" />Fresh Fruit</a>
-                                    </li>
+                                    @endforeach
                                 </ul>
                                 <ul class="end">
+                                    @foreach ($categories as $item)
+
+
                                     <li>
-                                        <a href="shop-grid-right.html"> <img src="{{ asset('frontend/assets/imgs/theme/icons/category-6.svg') }}" alt="" />Wines & Drinks</a>
+                                        <a href="shop-grid-right.html"> <img src="{{ asset($item->category_image) }}" alt="" />{{$item->category_name  }}</a>
                                     </li>
-                                    <li>
-                                        <a href="shop-grid-right.html"> <img src="{{ asset('frontend/assets/imgs/theme/icons/category-7.svg') }}" alt="" />Fresh Seafood</a>
-                                    </li>
-                                    <li>
-                                        <a href="shop-grid-right.html"> <img src="{{ asset('frontend/assets/imgs/theme/icons/category-8.svg') }}" alt="" />Fast food</a>
-                                    </li>
-                                    <li>
-                                        <a href="shop-grid-right.html"> <img src="{{ asset('frontend/assets/imgs/theme/icons/category-9.svg') }}" alt="" />Vegetables</a>
-                                    </li>
-                                    <li>
-                                        <a href="shop-grid-right.html"> <img src="{{ asset('frontend/assets/imgs/theme/icons/category-10.svg') }}" alt="" />Bread and Juice</a>
-                                    </li>
+                                    @endforeach
                                 </ul>
                             </div>
                             <div class="more_slide_open" style="display: none">
