47c3563d118ced7ce797ca05aab2162152d880ac
Md Tipu Sultan <greenviewsoft@gmail.com>
2022-12-05 17:33:51 +0600

    Frontend All Special Deals,Hot Deals, Product Show

--- app/Http/Controllers/Frontend/IndexController.php
+++ app/Http/Controllers/Frontend/IndexController.php
@@ -18,7 +18,28 @@ public function Index()
     $skip_category_category_0 = Category::skip(3)->first();
     $skip_product_0 = Product::where('status',1)->where('category_id',$skip_category_category_0->id)->orderBy('id','DESC')->limit(5)->get();
 
-    return view('frontend.index',compact('skip_category_category_0','skip_product_0'));
+
+   $skip_category_category_2 = Category::skip(5)->first();
+   $skip_product_2 = Product::where('status',1)->where('category_id',$skip_category_category_2->id)->orderBy('id','DESC')->limit(5)->get();
+
+
+   $skip_category_category_3 = Category::skip(1)->first();
+   $skip_product_3 = Product::where('status',1)->where('category_id',$skip_category_category_3->id)->orderBy('id','DESC')->limit(5)->get();
+
+   $skip_category_category_4 = Category::skip(10)->first();
+   $skip_product_4 = Product::where('status',1)->where('category_id',$skip_category_category_4->id)->orderBy('id','DESC')->limit(5)->get();
+
+
+   $hot_deals = Product::where('hot_deals',1)->where('discount_price','!=',NULL)->orderBy('id','DESC')->limit(3)->get();
+
+   $special_offer = Product::where('special_offer',1)->orderBy('id','DESC')->limit(3)->get();
+
+   $recently_deals = Product::where('status',1)->orderBy('id','DESC')->limit(3)->get();
+
+   $special_deals = Product::where('special_deals',1)->orderBy('id','DESC')->limit(3)->get();
+
+
+    return view('frontend.index',compact('skip_category_category_0','skip_product_0','skip_category_category_2','skip_product_2','skip_category_category_3','skip_product_3', 'skip_category_category_4','skip_product_4','hot_deals','special_offer','recently_deals','special_deals'));
 }
 
 
